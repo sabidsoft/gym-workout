c7a7880a7d11d20d6d4fcb2545a8f1e5949e5a33
Sabid Hasan <sabid.soft@gmail.com>
2022-09-28 23:04:26 +0600

    exercises.json file and exercise component added

---
+++ src/components/Exercise/Exercise.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Exercise.css'
+
+const Exercise = () => {
+    return (
+        <div>
+            <h1>Exercise</h1>
+        </div>
+    );
+};
+
+export default Exercise;
